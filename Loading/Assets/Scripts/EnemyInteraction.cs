9881c67a868969831b61fc7124119a357c720139
Grant Walker <gwwalker@uci.edu>
2015-01-24 00:46:30 -0800

    Started to add the scripts we need to make the AI work and started to set up the UI

---
+++ Loading/Assets/Scripts/EnemyInteraction.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EnemyInteraction : MonoBehaviour 
+{
+	public Canvas ui;
+	public Text choice1;
+	public Text choice2;
+
+	// Use this for initialization
+	void Start () 
+	{
+		ui.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter () 
+	{
+		ui.enabled = true;
+		Debug.Log("Player hit the AI");
+		choice1.text = "Hello";
+		choice2.text = "Good Bye";
+		GameController.Instance.SendMessage("AddSanity");
+	}
+}
---
+++ Loading/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+
+	public static GameController Instance;
+	
+	void Awake() 
+	{
+		Instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void AddSanity()
+	{
+		Debug.Log("Message got here");
+	}
+}
