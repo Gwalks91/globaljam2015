9d63d76e9e5339128a08c4040aba23ef9d57fa0b
Grant Walker <gwwalker@uci.edu>
2015-01-23 23:05:13 -0800

    Initial Commit lets see if this works

---
+++ Loading/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+	private bool facingRight = true;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		float move = Input.GetAxis("Horizontal");
+
+		rigidbody.velocity = new Vector3(move * maxSpeed, rigidbody.velocity.y, 0);
+	}
+
+
+}
